f5e53436182a9bb22a706a391a953b45854cc499
gamerer <starkid2u@outlook.com>
2021-09-07 21:35:03 -0400

    remove hamon.py because i cant be bothered to add cogs

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from discord_slash.utils.manage_commands import create_permission
 from discord_slash.model import ButtonStyle, SlashCommandPermissionType
 
 from extra import makestats, addtoinv, changestats, checkmoney, changemoney, getinv, getstats, removefrominv, givehamon
-import hamon
+from hamon import heal
 
 import random
 import json
@@ -213,5 +213,65 @@ async def stand(ctx):
 	stats = await getstats(ctx.author)
 	embednone = discord.Embed(title=f"stand", colour=discord.Colour(0x16eb4), description=f"you dont have a stand")
 	embednone = discord.Embed(title=f"stand", colour=discord.Colour(0x16eb4), description=f"")
+
+@slash.slash(description="hamon heal", permissions={880620607102935091: [create_permission(884220480465305600, SlashCommandPermissionType.ROLE, False)]})
+@commands.cooldown(rate=1,per=3600,type=commands.BucketType.user)
+async def heal(ctx, user:discord.Member = None):
+
+	authorstats = await getstats(ctx.author)
+
+	if user is None:
+		stats = await getstats(ctx.author)
+		user = ctx.author
+	else:
+		stats = await getstats(user)
+		print(stats)
+
+	if authorstats["hamon type"] == None:
+		await ctx.send(embed=discord.Embed(title=f"healing", colour=discord.Colour(0x16eb4), description=f"you dont know hamon!"), hidden=True)
+		return
+
+	if authorstats["hamon type"] != "healing" and ctx.author != user:
+		await ctx.send(embed=discord.Embed(title=f"healing", colour=discord.Colour(0x16eb4), description=f"you dont know the right type of hamon to heal someone else"), hidden=True)
+	elif authorstats["hamon level"] < 2 and ctx.author != user:
+		await ctx.send(embed=discord.Embed(title=f"healing", colour=discord.Colour(0x16eb4), description=f"you arent a high enough level to heal someone else"), hidden=True)
+	elif authorstats["hamon level"] >= 2 and ctx.author != user:
+		change = stats
+
+		amountmin = stats["hamon level"] * 3
+		amountmax = stats["hamon level"] * 6
+		amount = random.randrange(amountmin, amountmax)
+
+		change["hp"] += amount
+
+		if change["hp"] > stats["max hp"]:
+			change["hp"] = stats["max hp"]
+
+		await changestats(ctx, user, change)
+
+		embed = discord.Embed(title=f"healing", colour=discord.Colour(0x16eb4), description=f"healed **{user.display_name}** for **{amount}**")
+		await ctx.send(embed=embed, hidden=True)
+	elif ctx.author == user:
+		change = stats
+
+		if stats["hamon type"] == "healing":
+			amountmin = stats["hamon level"] * 3 + stats["hamon level"] * 2
+			amountmax = stats["hamon level"] * 6 + stats["hamon level"] * 3
+		else:
+			amountmin = stats["hamon level"] * 3
+			amountmax = stats["hamon level"] * 6 
+		
+		amount = random.randrange(amountmin, amountmax)
+
+		change["hp"] += amount
+
+		if change["hp"] > stats["max hp"]:
+			change["hp"] = stats["max hp"]
+
+		await changestats(ctx=ctx, user=ctx.author, change=change)
+
+		embed = discord.Embed(title=f"healing", colour=discord.Colour(0x16eb4), description=f"healed yourself for **{amount}**")
+		await ctx.send(embed=embed, hidden=True)
+
 
 client.run(token)
